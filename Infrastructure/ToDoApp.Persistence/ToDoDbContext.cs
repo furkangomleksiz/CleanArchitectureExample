745b6d0054e6cea4640240ea5ab14bcb3838fd25
furkangomleksiz <furkangomleksiz@gmail.com>
2024-07-05 14:05:01 +0300

    Sqlite Repository

---
+++ Infrastructure/ToDoApp.Persistence/ToDoDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Persistence
+{
+    public class ToDoDbContext : DbContext
+    {
+
+        public ToDoDbContext()
+        {
+        }
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+        :base(options)
+        {
+        }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
--- Presentation/ToDoApp.API/Program.cs
+++ Presentation/ToDoApp.API/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoApp.Application;
 using ToDoApp.Application.Queries;
 using ToDoApp.Domain.Interfaces;
+using ToDoApp.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +13,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ToDoItemQueryHandler).Assembly));
-builder.Services.AddSingleton<IToDoRepository, InMemoryToDoRepository>();
+builder.Services.AddScoped<IToDoRepository, SqlToDoRepository>();
 
 var app = builder.Build();
 
